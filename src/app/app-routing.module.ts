75fb69f81af29513a47b4dc3e46f2c75842ce837
leenawat <leenawat.pa@gmail.com>
2020-05-06 16:30:30 +0700

    add login, main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: MainComponent
+}, {
+  path: 'login', component: LoginComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
